49d9a5347cad73d0e3d24f790135e921258cb2d8
ToYama <100946113+ToYama170402@users.noreply.github.com>
2024-08-22 14:46:40 +0900

    GTMの設置 (#12)

--- rishu-app/app/layout.tsx
+++ rishu-app/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google"
 
 export const metadata: Metadata = {
   title: "rishu-app",
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>{children}</body>
+      <GoogleTagManager gtmId="GTM-M47VN8WW" />
     </html>
   );
 }

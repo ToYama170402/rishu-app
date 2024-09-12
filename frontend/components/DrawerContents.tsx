2af59d59f660ebcf47d90aa738feff66def4a0b8
ToYama170402 <100946113+ToYama170402@users.noreply.github.com>
2024-09-12 16:02:37 +0900

    リファクタとデザインの微修正

---
+++ frontend/components/DrawerContents.tsx
@@ -0,0 +1,5 @@
+import { Box } from "@mui/material";
+
+export default function DrawerContent(): React.ReactNode {
+  return <Box>hello world</Box>;
+}
--- frontend/components/SideBar.tsx
+++ frontend/components/SideBar.tsx
@@ -10,15 +10,15 @@ import {
   SwipeableDrawer,
   Toolbar as ToolBar,
   Typography,
+  Button,
 } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import HelpIcon from "@mui/icons-material/Help";
 import MenuIcon from "@mui/icons-material/Menu";
 import MenuOpen from "@mui/icons-material/MenuOpen";
 import SettingsIcon from "@mui/icons-material/Settings";
-function DrawerContent(): React.ReactNode {
-  return <>hello world</>;
-}
+import DrawerContents from "./DrawerContents";
+
 export default function Layout({
   children,
 }: {
@@ -26,30 +26,134 @@ export default function Layout({
 }): JSX.Element {
   const theme = useTheme();
   const isPC = useMediaQuery(theme.breakpoints.up("sm"));
-  console.log(`isPC: ${isPC}`);
-  const [open, setOpen] = useState(isPC ? true : false);
+
+  const [open, setOpen] = useState(isPC);
   const [drawerWidth, setDrawerWidth] = useState("160px");
+
   const handleDrawerOpen = () => {
-    open ? setOpen(false) : setOpen(true);
-    setDrawerWidth(open ? "160px" : "48px");
+    setOpen(!open);
+    setDrawerWidth(open ? "160px" : "56px");
   };
+
+  const PcSideBar = (): React.ReactNode => {
+    return (
+      <>
+        <Paper
+          sx={{
+            width: drawerWidth,
+            display: isPC ? "block" : "none",
+            overflow: "hidden",
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.primary.contrastText,
+            borderRadius: "0px 4px 4px 0px",
+          }}
+        >
+          <Box p={1} height={"100%"}>
+            <Stack direction="row" justifyContent="space-between">
+              <IconButton onClick={handleDrawerOpen} sx={{ color: "inherit" }}>
+                <MenuIcon sx={{ display: "block" }} />
+              </IconButton>
+              <Box p={1}>
+                <Typography variant="h6" variantMapping={{ h6: "h1" }} noWrap>
+                  rishu-app
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack
+              direction={"column"}
+              justifyContent={"space-between"}
+              height={"calc(100% - 48px)"}
+            >
+              <DrawerContents />
+              <Box>
+                <Button
+                  variant={"text"}
+                  startIcon={<HelpIcon />}
+                  sx={{
+                    textWrap: "nowrap",
+                    color: theme.palette.primary.contrastText,
+                  }}
+                >
+                  ヘルプ
+                </Button>
+                <Button
+                  variant={"text"}
+                  startIcon={<SettingsIcon />}
+                  sx={{
+                    textWrap: "nowrap",
+                    color: theme.palette.primary.contrastText,
+                  }}
+                >
+                  設定
+                </Button>
+              </Box>
+            </Stack>
+          </Box>
+        </Paper>
+      </>
+    );
+  };
+
+  const MobileSideBar = (): React.ReactNode => {
+    return (
+      <>
+        <AppBar
+          sx={{
+            position: "static",
+            display: isPC ? "none" : "block",
+            color: theme.palette.primary.contrastText,
+            borderRadius: "4px 4px 0px 0px",
+          }}
+        >
+          <ToolBar>
+            <Stack
+              direction={"row"}
+              justifyContent={"space-between"}
+              width={"100%"}
+            >
+              <Box p={1}>
+                <Typography variant="h6" variantMapping={{ h6: "h1" }}>
+                  rishu-app
+                </Typography>
+              </Box>
+              <Stack direction={"row"} alignItems={"center"}>
+                <IconButton>
+                  <HelpIcon
+                    sx={{ color: theme.palette.primary.contrastText }}
+                  />
+                </IconButton>
+                <IconButton>
+                  <SettingsIcon
+                    sx={{ color: theme.palette.primary.contrastText }}
+                  />
+                </IconButton>
+                <IconButton onClick={handleDrawerOpen}>
+                  <MenuOpen
+                    sx={{ color: theme.palette.primary.contrastText }}
+                  />
+                </IconButton>
+              </Stack>
+            </Stack>
+          </ToolBar>
+        </AppBar>
+        <SwipeableDrawer
+          onClose={handleDrawerOpen}
+          onOpen={handleDrawerOpen}
+          anchor="right"
+          open={open}
+          sx={{ display: isPC ? "none" : "block", width: "80vw" }}
+        >
+          <Box p={1} sx={{ width: "80vw" }}>
+            <DrawerContents />
+          </Box>
+        </SwipeableDrawer>
+      </>
+    );
+  };
+
   return (
     <Stack direction={isPC ? "row" : "column"}>
-      <Paper sx={{ width: drawerWidth, display: isPC ? "block" : "none" }}>
-        <Box p={1}>
-          <Stack direction="row" justifyContent="space-between">
-            <IconButton onClick={handleDrawerOpen}>
-              <MenuIcon sx={{ display: "block" }} />
-            </IconButton>
-            <Box p={1}>
-              <Typography variant="h6" variantMapping={{ h6: "h1" }} noWrap>
-                rishu-app
-              </Typography>
-            </Box>
-          </Stack>
-          <DrawerContent />
-        </Box>
-      </Paper>
+      <PcSideBar />
       <Box
         sx={{
           width: `calc(100% - ${isPC ? drawerWidth : "0px"})`,
@@ -58,42 +162,7 @@ export default function Layout({
       >
         {children}
       </Box>
-      <AppBar sx={{ position: "static" }}>
-        <ToolBar>
-          <Stack
-            direction={"row"}
-            justifyContent={"space-between"}
-            width={"100%"}
-          >
-            <Box p={1}>
-              <Typography variant="h6" variantMapping={{ h6: "h1" }}>
-                rishu-app
-              </Typography>
-            </Box>
-            <Stack direction={"row"} alignItems={"center"}>
-              <IconButton>
-                <HelpIcon />
-              </IconButton>
-              <IconButton>
-                <SettingsIcon />
-              </IconButton>
-              <IconButton onClick={handleDrawerOpen}>
-                <MenuOpen />
-              </IconButton>
-            </Stack>
-          </Stack>
-        </ToolBar>
-      </AppBar>
-      <SwipeableDrawer
-        onClose={() => {}}
-        onOpen={() => {}}
-        anchor="right"
-        open={open}
-        onClick={handleDrawerOpen}
-        sx={{ display: isPC ? "none" : "block" }}
-      >
-        <DrawerContent />
-      </SwipeableDrawer>
+      <MobileSideBar />
     </Stack>
   );
 }

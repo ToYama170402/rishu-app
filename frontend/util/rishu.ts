5b4e76c927ea82b346ce8034b0fb48986bd2900b
ToYama <100946113+ToYama170402@users.noreply.github.com>
2024-09-17 07:35:06 +0900

    fetchを修正 (#24)

--- frontend/util/rishu.ts
+++ frontend/util/rishu.ts
@@ -1,17 +1,18 @@
 import { parseSV } from "./helper";
 async function fetchAll() {
   try {
-    const response = await fetch('https://kurisyushien.org/api?mode=&word=', {
-      mode: 'cors',
-      redirect: 'follow',
+    const response = await fetch("https://kurisyushien.org/api?mode=&word=", {
+      mode: "cors",
+      redirect: "follow",
+      next: { revalidate: 50 },
     });
     if (!response.ok) {
-      throw new Error('Failed to fetch data');
+      throw new Error("Failed to fetch data");
     }
     const data = await response.json();
-    return parseSV(data, '\t');
+    return parseSV(data, "\t");
   } catch (error) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
 }
 export { fetchAll };

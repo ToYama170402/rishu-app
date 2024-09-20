b8434aeb28358b443e69debc91f9ab09b82958b9
ToYama170402 <100946113+ToYama170402@users.noreply.github.com>
2024-09-20 19:21:16 +0900

    色々修正

--- frontend/components/DetailPopup.tsx
+++ frontend/components/DetailPopup.tsx
@@ -23,13 +23,12 @@ import {
   Typography,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import React, { useEffect } from "react";
 import {
   applicantsAmount,
   calcApplicantsRatio,
   lecture,
 } from "../util/timeTable";
-import { useTheme } from "@emotion/react";
+import React from "react";
 
 function date2number(day: string): number {
   switch (day) {
@@ -70,6 +69,7 @@ function ApplicantsBar({
           position={"absolute"}
           top={0}
           left={0}
+          flexShrink={0}
           border="1px dashed black"
           sx={{ zIndex: 1, backgroundColor: "#00000000" }}
         ></Box>
@@ -82,8 +82,13 @@ function ApplicantsBar({
           height={"100%"}
           position={"absolute"}
           top={0}
+          flexShrink={0}
           right={0}
-          sx={{ zIndex: 1, backgroundColor: colors.red[100], opacity: 0.6 }}
+          sx={{
+            zIndex: 1,
+            opacity: 0.8,
+            backgroundImage: `repeating-linear-gradient(60deg, #00000000, ${colors.red[100]} 0px 5px, #00000000 5px 10px)`,
+          }}
         ></Box>
         <Stack
           direction={"row"}
@@ -99,22 +104,31 @@ function ApplicantsBar({
           <Box
             width={
               applicantsAmount.all > capacity
-                ? (capacity / applicantsAmount.all) * 100 + "%"
-                : "100%"
-            }
-            sx={{ backgroundColor: colors.amber[100] }}
-          >
-            {applicantsAmount.primary ? applicantsAmount.primary : null}
-          </Box>
-          <Box
-            width={
-              applicantsAmount.all > capacity
                 ? (applicantsAmount.first / applicantsAmount.all) * 100 + "%"
                 : (applicantsAmount.first / capacity) * 100 + "%"
             }
+            flexShrink={0}
             sx={{ bgcolor: colors.blue[100] }}
           >
             {applicantsAmount.first ? applicantsAmount.first : null}
+            <Box
+              position={"absolute"}
+              top={0}
+              left={0}
+              height={"50%"}
+              fontSize={"0.5rem"}
+              width={
+                applicantsAmount.all > capacity
+                  ? (applicantsAmount.primary / applicantsAmount.all) * 100 +
+                    "%"
+                  : (applicantsAmount.primary / capacity) * 100 + "%"
+              }
+              flexShrink={0}
+              sx={{ textWrap: "nowrap", backgroundColor: colors.amber[100] }}
+            >
+              優先指定：
+              {applicantsAmount.primary ? applicantsAmount.primary : null}
+            </Box>
           </Box>
           <Box
             width={
@@ -122,6 +136,7 @@ function ApplicantsBar({
                 ? (applicantsAmount.second / applicantsAmount.all) * 100 + "%"
                 : (applicantsAmount.second / capacity) * 100 + "%"
             }
+            flexShrink={0}
             sx={{ bgcolor: colors.orange[100] }}
           >
             {applicantsAmount.second ? applicantsAmount.second : null}
@@ -132,6 +147,7 @@ function ApplicantsBar({
                 ? (applicantsAmount.third / applicantsAmount.all) * 100 + "%"
                 : (applicantsAmount.third / capacity) * 100 + "%"
             }
+            flexShrink={0}
             sx={{ bgcolor: colors.green[100] }}
           >
             {applicantsAmount.third ? applicantsAmount.third : null}
@@ -142,6 +158,7 @@ function ApplicantsBar({
                 ? (applicantsAmount.forth / applicantsAmount.all) * 100 + "%"
                 : (applicantsAmount.forth / capacity) * 100 + "%"
             }
+            flexShrink={0}
             sx={{ bgcolor: colors.purple[100] }}
           >
             {applicantsAmount.forth ? applicantsAmount.forth : null}
@@ -152,6 +169,7 @@ function ApplicantsBar({
                 ? (applicantsAmount.fifth / applicantsAmount.all) * 100 + "%"
                 : (applicantsAmount.fifth / capacity) * 100 + "%"
             }
+            flexShrink={0}
             sx={{ bgcolor: colors.grey[100] }}
           >
             {applicantsAmount.fifth ? applicantsAmount.fifth : null}
@@ -184,20 +202,16 @@ export default function DetailPopup({
   //   });
   // }, [lecture.number]);
 
-  const applicantsRatio = calcApplicantsRatio(
-    lecture.capacity,
-    lecture.applicants
-  ).map((ratio) => Math.round(ratio * 100) + "%");
-
   const StyledTableRow = styled(TableRow)(({ theme }) => ({
     "&:nth-of-type(odd)": {
       backgroundColor: theme.palette.action.hover,
     },
-    // hide last border
-    "&:last-child td, &:last-child th": {
-      border: 0,
-    },
   }));
+
+  const applicantsRatio = calcApplicantsRatio(
+    lecture.capacity,
+    lecture.applicants
+  ).map((ratio) => Math.round(ratio * 100) + "%");
 
   return (
     <Dialog open={open} onClose={onClose} fullWidth maxWidth="sm">
@@ -248,7 +262,7 @@ export default function DetailPopup({
               <TableContainer sx={{ textWrap: "nowrap" }} component={Paper}>
                 <Table size="small">
                   <TableHead>
-                    <TableRow sx={{ backgroundColor: colors.blue[100] }}>
+                    <TableRow>
                       <TableCell>希望順位</TableCell>
                       <TableCell align="right">人数</TableCell>
                       <TableCell align="right">当選確率</TableCell>
@@ -256,42 +270,145 @@ export default function DetailPopup({
                   </TableHead>
                   <TableBody>
                     <StyledTableRow>
-                      <TableCell>優先指定</TableCell>
+                      <TableCell>
+                        <Box sx={{ position: "relative " }}>
+                          <Box position={"relative"} zIndex={1}>
+                            優先指定
+                          </Box>
+                          <Box
+                            width={"100%"}
+                            height={"50%"}
+                            position={"absolute"}
+                            top={"50%"}
+                            left={0}
+                            zIndex={0}
+                            sx={{
+                              backgroundImage: `repeating-linear-gradient(135deg, #00000000, ${colors.amber[100]} 0px 5px, #00000000 5px 10px)`,
+                            }}
+                          ></Box>
+                        </Box>
+                      </TableCell>
                       <TableCell align="right">
                         {lecture.applicants.primary}
                       </TableCell>
                       <TableCell align="right">{applicantsRatio[0]}</TableCell>
                     </StyledTableRow>
                     <StyledTableRow>
-                      <TableCell>第１希望</TableCell>
+                      <TableCell>
+                        <Box sx={{ position: "relative " }}>
+                          <Box position={"relative"} zIndex={1}>
+                            第１希望
+                          </Box>
+                          <Box
+                            width={"100%"}
+                            height={"50%"}
+                            position={"absolute"}
+                            top={"50%"}
+                            left={0}
+                            zIndex={0}
+                            sx={{
+                              backgroundImage: `repeating-linear-gradient(135deg, #00000000, ${colors.blue[100]} 0px 5px, #00000000 5px 10px)`,
+                            }}
+                          ></Box>
+                        </Box>
+                      </TableCell>
                       <TableCell align="right">
                         {lecture.applicants.first}
                       </TableCell>
                       <TableCell align="right">{applicantsRatio[1]}</TableCell>
                     </StyledTableRow>
                     <StyledTableRow>
-                      <TableCell>第２希望</TableCell>
+                      <TableCell>
+                        <Box sx={{ position: "relative " }}>
+                          <Box position={"relative"} zIndex={1}>
+                            第2希望
+                          </Box>
+                          <Box
+                            width={"100%"}
+                            height={"50%"}
+                            position={"absolute"}
+                            top={"50%"}
+                            left={0}
+                            zIndex={0}
+                            sx={{
+                              backgroundImage: `repeating-linear-gradient(135deg, #00000000, ${colors.orange[100]} 0px 5px, #00000000 5px 10px)`,
+                            }}
+                          ></Box>
+                        </Box>
+                      </TableCell>
                       <TableCell align="right">
                         {lecture.applicants.second}
                       </TableCell>
                       <TableCell align="right">{applicantsRatio[2]}</TableCell>
                     </StyledTableRow>
                     <StyledTableRow>
-                      <TableCell>第３希望</TableCell>
+                      <TableCell>
+                        <Box sx={{ position: "relative " }}>
+                          <Box position={"relative"} zIndex={1}>
+                            第３希望
+                          </Box>
+                          <Box
+                            width={"100%"}
+                            height={"50%"}
+                            position={"absolute"}
+                            top={"50%"}
+                            left={0}
+                            zIndex={0}
+                            sx={{
+                              backgroundImage: `repeating-linear-gradient(135deg, #00000000, ${colors.green[100]} 0px 5px, #00000000 5px 10px)`,
+                            }}
+                          ></Box>
+                        </Box>
+                      </TableCell>
                       <TableCell align="right">
                         {lecture.applicants.third}
                       </TableCell>
                       <TableCell align="right">{applicantsRatio[3]}</TableCell>
                     </StyledTableRow>
                     <StyledTableRow>
-                      <TableCell>第４希望</TableCell>
+                      <TableCell>
+                        <Box sx={{ position: "relative " }}>
+                          <Box position={"relative"} zIndex={1}>
+                            第４希望
+                          </Box>
+                          <Box
+                            width={"100%"}
+                            height={"50%"}
+                            position={"absolute"}
+                            top={"50%"}
+                            left={0}
+                            zIndex={0}
+                            sx={{
+                              backgroundImage: `repeating-linear-gradient(135deg, #00000000, ${colors.purple[100]} 0px 5px, #00000000 5px 10px)`,
+                            }}
+                          ></Box>
+                        </Box>
+                      </TableCell>
                       <TableCell align="right">
                         {lecture.applicants.forth}
                       </TableCell>
                       <TableCell align="right">{applicantsRatio[4]}</TableCell>
                     </StyledTableRow>
                     <StyledTableRow>
-                      <TableCell>第５希望</TableCell>
+                      <TableCell>
+                        {" "}
+                        <Box sx={{ position: "relative " }}>
+                          <Box position={"relative"} zIndex={1}>
+                            第５希望
+                          </Box>
+                          <Box
+                            width={"100%"}
+                            height={"50%"}
+                            position={"absolute"}
+                            top={"50%"}
+                            left={0}
+                            zIndex={0}
+                            sx={{
+                              backgroundImage: `repeating-linear-gradient(135deg, #00000000, ${colors.grey[100]} 0px 5px, #00000000 5px 10px)`,
+                            }}
+                          ></Box>
+                        </Box>
+                      </TableCell>
                       <TableCell align="right">
                         {lecture.applicants.fifth}
                       </TableCell>

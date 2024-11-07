33be18d60a719b4e7120b5d8e52031f28a79da3c
ToYama170402 <100946113+ToYama170402@users.noreply.github.com>
2024-11-08 00:59:11 +0900

    処理を色々修正

--- backend/api/rishu_scraping.py
+++ backend/api/rishu_scraping.py
@@ -6,56 +6,68 @@ import os
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def scrapeRegistrationStatus() -> List[List[str]]:
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--disable-gpu")
+    logger.info("connecting selenium")
+    time = datetime.now()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--log-level=3")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Remote(
-        command_executor=os.environ["SELENIUM_URL"], options=webdriver.ChromeOptions()
+        command_executor=os.environ["SELENIUM_URL"], options=chrome_options
     )
-    print("Accessing the target page...")
+    logger.info(f"access selenium: {datetime.now() - time}")
+    logger.info("Accessing the target page...")
+    driver.get("about:blank")
+    driver.delete_all_cookies()
     driver.get(
         "https://eduweb.sta.kanazawa-u.ac.jp/portal/Public/Regist/RegistrationStatus.aspx?year=2024&lct_term_cd=21"
     )
 
-    # 期間外の場合は終了
-    check_out_of_term = driver.find_element(
-        By.ID, "ctl00_phContents_ucRegistrationStatus_ucEmptyBox_lbl_lbl"
-    ).text
-    if check_out_of_term == "期間外対象となるデータは存在しません。":
-        print("Out of term")
-        return []
-
-    # ドロップダウンリストの要素を取得
-    print("Selecting the dropdown list...")
-    dropdown = Select(
-        driver.find_element(By.ID, "ctl00_phContents_ucRegistrationStatus_ddlLns_ddl")
-    )
-    # オプションを全件に変更
-    print("Changing the dropdown list to 'All'...")
-    dropdown.select_by_index(0)
-
-    # テーブル行の要素を取得
-    print("Getting the table rows...")
-    records = driver.find_elements(
-        By.CSS_SELECTOR, "#ctl00_phContents_ucRegistrationStatus_tbGridView table tr"
-    )
-
-    # 各行のすべての列を取得
-    arr = list(
-        map(
-            lambda record: list(
-                map(
-                    lambda column: column.text,
-                    record.find_elements(By.CSS_SELECTOR, "td, th"),
-                )
-            ),
-            records,
+    try:
+        # ドロップダウンリストの要素を取得
+        logger.info("Selecting the dropdown list...")
+        dropdown = Select(
+            driver.find_element(
+                By.ID, "ctl00_phContents_ucRegistrationStatus_ddlLns_ddl"
+            )
         )
-    )
-    return arr
+        # オプションを全件に変更
+        logger.info("Changing the dropdown list to 'All'...")
+        dropdown.select_by_index(0)
+
+        # テーブル行の要素を取得
+        logger.info("Getting the table rows...")
+        records = driver.find_elements(
+            By.CSS_SELECTOR,
+            "#ctl00_phContents_ucRegistrationStatus_tbGridView table tr",
+        )
+
+        # 各行のすべての列を取得
+        arr = list(
+            map(
+                lambda record: list(
+                    map(
+                        lambda column: column.text,
+                        record.find_elements(By.CSS_SELECTOR, "td, th"),
+                    )
+                ),
+                records,
+            )
+        )
+        driver.quit()
+        return arr
+
+    except Exception as e:
+        driver.quit()
+        raise e
 
 
 def scrapeSyllabus(course_numbering: str):

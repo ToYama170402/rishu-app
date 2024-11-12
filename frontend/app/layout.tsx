97d11d0ee2c7afe4e57b513480bc98ba339aa5e6
ToYama <toyamasoujinn@gamil.com>
2024-11-12 23:07:56 +0900

    search consoleに対応 GTMとsearch consoleに必要なトークンを環境変数化

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,6 +5,9 @@ import { GoogleTagManager } from "@next/third-parties/google";
 export const metadata: Metadata = {
   title: "rishu-app",
   description: "金沢大学抽選対象科目履修登録支援WEBアプリ",
+  verification: {
+    google: process.env.GOOGLE_SITE_VERIFICATION,
+  },
 };
 
 export default function RootLayout({
@@ -15,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body style={{ height: "100dvh" }}>{children}</body>
-      <GoogleTagManager gtmId="GTM-M47VN8WW" />
+      <GoogleTagManager gtmId={process.env.GTM!} />
     </html>
   );
 }

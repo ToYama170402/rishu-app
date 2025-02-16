54f7f70252267a4adfaf02973b8eb4e7e551be00
ToYama <toyamasoujinn@gamil.com>
2025-02-16 01:53:33 +0000

    DetaipPopupをファイル分割

--- frontend/src/features/DetailPopup/DetailPopup.tsx
+++ frontend/src/features/DetailPopup/DetailPopup.tsx
@@ -5,30 +5,18 @@ import {
   Box,
   Button,
   ButtonGroup,
-  colors,
   Dialog,
   DialogContent,
   DialogTitle,
   Divider,
-  Paper,
   Stack,
   Tab,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
   Typography,
 } from "@mui/material";
-import { styled } from "@mui/material/styles";
 import React from "react";
-import {
-  applicantsAmount,
-  calcApplicantsRatio,
-  lecture,
-} from "@/util/timeTable";
+import { lecture } from "@/util/timeTable";
 import ApplicantsBar from "@/components/ApplicantsBar/ApplicantsBar";
+import LectureDetailTable from "./components/LectureDetailTable";
 
 export default function DetailPopup({
   lecture,
@@ -43,25 +31,6 @@ export default function DetailPopup({
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
-
-  // const [lectureDetail, setLectureDetail] = React.useState(null);
-  // useEffect(() => {
-  //   fetchDetail(lecture.number).then((data) => {
-  //     console.log(data);
-  //     setLectureDetail(data);
-  //   });
-  // }, [lecture.number]);
-
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
-    },
-  }));
-
-  const applicantsRatio = calcApplicantsRatio(
-    lecture.capacity,
-    lecture.applicants
-  ).map((ratio) => Math.round(ratio * 100) + "%");
 
   return (
     <Dialog open={open} onClose={onClose} fullWidth maxWidth="sm">
@@ -111,193 +80,7 @@ export default function DetailPopup({
               {lecture.applicants.all > 0 ? (
                 <ApplicantsBar lecture={lecture} base="allApplicants" />
               ) : null}
-              <TableContainer sx={{ textWrap: "nowrap" }} component={Paper}>
-                <Table size="small">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>希望順位</TableCell>
-                      <TableCell align="right">人数</TableCell>
-                      <TableCell align="right">当選確率</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    <StyledTableRow>
-                      <TableCell>
-                        <Box
-                          sx={{ position: "relative " }}
-                          width={"fit-content"}
-                        >
-                          <Box position={"relative"} zIndex={1}>
-                            優先指定
-                          </Box>
-                          <Box
-                            width={"100%"}
-                            height={"50%"}
-                            position={"absolute"}
-                            top={"50%"}
-                            left={0}
-                            zIndex={0}
-                            sx={{
-                              backgroundImage: `repeating-linear-gradient(135deg, #00000000, ${colors.amber[100]} 0px 5px, #00000000 5px 10px)`,
-                            }}
-                          ></Box>
-                        </Box>
-                      </TableCell>
-                      <TableCell align="right">
-                        {lecture.applicants.primary}
-                      </TableCell>
-                      <TableCell align="right">{applicantsRatio[0]}</TableCell>
-                    </StyledTableRow>
-                    <StyledTableRow>
-                      <TableCell>
-                        <Box
-                          sx={{ position: "relative " }}
-                          width={"fit-content"}
-                        >
-                          <Box position={"relative"} zIndex={1}>
-                            第１希望
-                          </Box>
-                          <Box
-                            width={"100%"}
-                            height={"50%"}
-                            position={"absolute"}
-                            top={"50%"}
-                            left={0}
-                            zIndex={0}
-                            sx={{
-                              backgroundImage: `repeating-linear-gradient(135deg, #00000000, ${colors.blue[100]} 0px 5px, #00000000 5px 10px)`,
-                            }}
-                          ></Box>
-                        </Box>
-                      </TableCell>
-                      <TableCell align="right">
-                        {lecture.applicants.first}
-                      </TableCell>
-                      <TableCell align="right">{applicantsRatio[1]}</TableCell>
-                    </StyledTableRow>
-                    <StyledTableRow>
-                      <TableCell>
-                        <Box
-                          sx={{ position: "relative " }}
-                          width={"fit-content"}
-                        >
-                          <Box position={"relative"} zIndex={1}>
-                            第2希望
-                          </Box>
-                          <Box
-                            width={"100%"}
-                            height={"50%"}
-                            position={"absolute"}
-                            top={"50%"}
-                            left={0}
-                            zIndex={0}
-                            sx={{
-                              backgroundImage: `repeating-linear-gradient(135deg, #00000000, ${colors.orange[100]} 0px 5px, #00000000 5px 10px)`,
-                            }}
-                          ></Box>
-                        </Box>
-                      </TableCell>
-                      <TableCell align="right">
-                        {lecture.applicants.second}
-                      </TableCell>
-                      <TableCell align="right">{applicantsRatio[2]}</TableCell>
-                    </StyledTableRow>
-                    <StyledTableRow>
-                      <TableCell>
-                        <Box
-                          sx={{ position: "relative " }}
-                          width={"fit-content"}
-                        >
-                          <Box position={"relative"} zIndex={1}>
-                            第３希望
-                          </Box>
-                          <Box
-                            width={"100%"}
-                            height={"50%"}
-                            position={"absolute"}
-                            top={"50%"}
-                            left={0}
-                            zIndex={0}
-                            sx={{
-                              backgroundImage: `repeating-linear-gradient(135deg, #00000000, ${colors.green[100]} 0px 5px, #00000000 5px 10px)`,
-                            }}
-                          ></Box>
-                        </Box>
-                      </TableCell>
-                      <TableCell align="right">
-                        {lecture.applicants.third}
-                      </TableCell>
-                      <TableCell align="right">{applicantsRatio[3]}</TableCell>
-                    </StyledTableRow>
-                    <StyledTableRow>
-                      <TableCell>
-                        <Box
-                          sx={{ position: "relative " }}
-                          width={"fit-content"}
-                        >
-                          <Box position={"relative"} zIndex={1}>
-                            第４希望
-                          </Box>
-                          <Box
-                            width={"100%"}
-                            height={"50%"}
-                            position={"absolute"}
-                            top={"50%"}
-                            left={0}
-                            zIndex={0}
-                            sx={{
-                              backgroundImage: `repeating-linear-gradient(135deg, #00000000, ${colors.purple[100]} 0px 5px, #00000000 5px 10px)`,
-                            }}
-                          ></Box>
-                        </Box>
-                      </TableCell>
-                      <TableCell align="right">
-                        {lecture.applicants.forth}
-                      </TableCell>
-                      <TableCell align="right">{applicantsRatio[4]}</TableCell>
-                    </StyledTableRow>
-                    <StyledTableRow>
-                      <TableCell>
-                        <Box
-                          sx={{ position: "relative " }}
-                          width={"fit-content"}
-                        >
-                          <Box position={"relative"} zIndex={1}>
-                            第５希望
-                          </Box>
-                          <Box
-                            width={"100%"}
-                            height={"50%"}
-                            position={"absolute"}
-                            top={"50%"}
-                            left={0}
-                            zIndex={0}
-                            sx={{
-                              backgroundImage: `repeating-linear-gradient(135deg, #00000000, ${colors.grey[100]} 0px 5px, #00000000 5px 10px)`,
-                            }}
-                          ></Box>
-                        </Box>
-                      </TableCell>
-                      <TableCell align="right">
-                        {lecture.applicants.fifth}
-                      </TableCell>
-                      <TableCell align="right">{applicantsRatio[5]}</TableCell>
-                    </StyledTableRow>
-                    <StyledTableRow sx={{ borderTopStyle: "double" }}>
-                      <TableCell>合計</TableCell>
-                      <TableCell align="right">
-                        {lecture.applicants.all}
-                      </TableCell>
-                      <TableCell align="right">-</TableCell>
-                    </StyledTableRow>
-                    <StyledTableRow>
-                      <TableCell>適正人数</TableCell>
-                      <TableCell align="right">{lecture.capacity}</TableCell>
-                      <TableCell align="right">-</TableCell>
-                    </StyledTableRow>
-                  </TableBody>
-                </Table>
-              </TableContainer>
+              <LectureDetailTable lecture={lecture} />
               <Box>
                 <Typography variant="body1">
                   ※当選確率はすべての優先指定が第１希望で指定されているものとして計算されています。

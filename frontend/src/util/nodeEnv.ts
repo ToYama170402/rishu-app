1f46c852a5fb8eac2ce333c5ff8bf936ee3e1a9b
ToYama <toyamasoujinn@gamil.com>
2025-02-16 17:09:29 +0900

    NODE_ENVが定義されていないとエラーが出るように修正

--- frontend/src/util/nodeEnv.ts
+++ frontend/src/util/nodeEnv.ts
@@ -1,4 +1,7 @@
 const nodeEnv = (): "development" | "production" => {
+  if (typeof process.env.NODE_ENV === "undefined") {
+    throw new Error("NODE_ENV is not defined");
+  }
   return process.env.NODE_ENV === "production" ? "production" : "development";
 };
 export default nodeEnv;

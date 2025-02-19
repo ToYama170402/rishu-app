1200635bbc037ad39cb57b9727604f4dd456a0b7
ToYama <100946113+ToYama170402@users.noreply.github.com>
2025-02-20 01:05:31 +0900

    Remove webpack setting (#75)
    * webpackの設定の削除
    
    * フォーマット
    
    ---------
    
    Co-authored-by: ToYama <toyamasoujinn@gamil.com>

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true,
-  webpack: (config, context) => {
-    config.watchOptions = {
-      ignored: /node_modules/,
-      poll: 8000,
-      aggregateTimeout: 300,
-    }
-    return config;
-  },
   output: "standalone",
 };
 

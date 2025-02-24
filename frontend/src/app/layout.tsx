cdaf7869622c13681371bb809dc19ebad7900f8e
ToYama <100946113+ToYama170402@users.noreply.github.com>
2025-02-24 22:13:06 +0900

    metaタグの追加 (#89)
    Co-authored-by: ToYama <toyamasoujinn@gamil.com>

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,8 +3,12 @@ import "./globals.css";
 import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
-  title: "rishu-app",
-  description: "金沢大学抽選対象科目履修登録支援WEBアプリ",
+  title: "rishu-app | 金沢大学履修登録支援ツール",
+  description:
+    "rishu-appは金沢大学生の履修登録を支援するツールです。抽選科目の応募状況を視覚的に確認したり、講義を探索することができます。",
+  authors: [{ name: "ToYama", url: "https://toyama170402.github.io/" }],
+  applicationName: "rishu-app",
+  formatDetection: { telephone: false, address: false, email: false },
   verification: {
     google: process.env.GOOGLE_SITE_VERIFICATION,
   },

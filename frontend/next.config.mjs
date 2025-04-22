a3761c6a503feeeb7c8d21cf5aedccf490a25261
ToYama <toyamasoujinn@gamil.com>
2025-04-22 15:02:27 +0000

    bundleAnalyzerの設定

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
   output: "standalone",
 };
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);

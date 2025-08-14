a82529169ab588bcf74983c1a76141702f565c42
ToYama170402 <yamato170402@gmail.com>
2025-08-14 11:03:21 +0000

    実際のシラバスに掲載されている情報の型

---
+++ syllabus-scrape/src/types/syllabusCourse.ts
@@ -0,0 +1,23 @@
+import type { Course } from "./course";
+import type { Department } from "./department";
+
+export type SyllabusCourse = Pick<
+  Course,
+  | "title"
+  | "instructors"
+  | "numbering"
+  | "courseNumber"
+  | "lectureForm"
+  | "numberOfProper"
+  | "schedule"
+  | "classFormat"
+  | "subjectDistinguished"
+  | "semester"
+  | "numberOfCredits"
+  | "max60CreditsFlag"
+  | "targetStudents"
+  | "keywords"
+  | "lectureRoomInfo"
+  | "openAccount"
+  | "note"
+> & { department: Department };

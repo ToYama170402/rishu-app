f910915cf0b10668067b01ba83ad81348f7281bf
ToYama170402 <yamato170402@gmail.com>
2025-08-14 10:14:13 +0900

    DBへの接続とマイグレーション関数の定義

---
+++ syllabus/src/internal/config/database.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func ConnectDatabase(host, port, user, password, dbname string) {
+	var err error
+	db, err = gorm.Open(postgres.Open(fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+}
+
+func MigrateDatabase(model interface{}) {
+	if db == nil {
+		log.Fatal("Database connection is not established")
+	}
+
+	db.AutoMigrate(&model)
+}
+
+func GetDB() *gorm.DB {
+	return db
+}

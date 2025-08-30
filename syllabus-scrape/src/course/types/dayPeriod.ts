c10e4fff7e9790ab2516d274a554b838f9bc4dec
ToYama170402 <yamato170402@gmail.com>
2025-08-30 08:27:59 +0000

    曜日時限を表現する型より厳密に表現するように修正

--- syllabus-scrape/src/course/types/dayPeriod.ts
+++ syllabus-scrape/src/course/types/dayPeriod.ts
@@ -1,4 +1,9 @@
-export type DayPeriod = {
-  day: "月" | "火" | "水" | "木" | "金" | "土" | "日" | "集中";
-  period: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | null;
-};
+export type DayPeriod =
+  | {
+      day: "月" | "火" | "水" | "木" | "金" | "土" | "日";
+      period: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+    }
+  | {
+      day: "集中";
+      period: null;
+    };

f6ecb4619b99154017717da86d2bfa9c8621c3d2
ToYama170402 <yamato170402@gmail.com>
2025-08-30 08:47:05 +0000

    曜日時限を構造化するパーサを実装

---
+++ syllabus-scrape/src/scraping/commonParser/dayTimeParser.ts
@@ -0,0 +1,33 @@
+import { type DayPeriod, isDayPeriod } from "@/course";
+import { zenToHan } from "@/utils/zenToHan";
+import { BaseParser, type ParseResult } from "../core";
+
+export class DayPeriodParser extends BaseParser<string, DayPeriod[]> {
+  parse(input: string): ParseResult<DayPeriod[]> {
+    const { result, duration } = this.measureParseTime(() => {
+      return zenToHan(input)
+        .trim()
+        .split(/[,、　 ]/)
+        .map((s) => {
+          const trimmed = s.trim();
+
+          if (trimmed === "集中") {
+            const dayPeriod = { day: "集中", period: null };
+            if (!isDayPeriod(dayPeriod)) {
+              throw new Error(`Invalid day/period format: ${trimmed}`);
+            }
+            return dayPeriod;
+          }
+
+          const day = trimmed[0];
+          const period = Number(trimmed[1]);
+          const dayPeriod = { day, period };
+          if (!isDayPeriod(dayPeriod)) {
+            throw new Error(`Invalid day/period format: ${s}`);
+          }
+          return dayPeriod;
+        });
+    });
+    return this.createParseResult(result, duration);
+  }
+}

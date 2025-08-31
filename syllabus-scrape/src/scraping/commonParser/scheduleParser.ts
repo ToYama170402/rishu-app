7de1453acb3951b717de14ce3b1a6c759a47e1a0
ToYama170402 <yamato170402@gmail.com>
2025-08-31 07:03:43 +0000

    月1~2のような形式に対応

--- syllabus-scrape/src/scraping/commonParser/scheduleParser.ts
+++ syllabus-scrape/src/scraping/commonParser/scheduleParser.ts
@@ -8,24 +8,48 @@ export class ScheduleParser extends BaseParser<string, DayPeriod[]> {
       return zenToHan(input)
         .trim()
         .split(/[,、　 ]/)
-        .map((s) => {
+        .flatMap<DayPeriod>((s) => {
           const trimmed = s.trim();
 
+          // 集中講義
           if (trimmed === "集中") {
             const dayPeriod = { day: "集中", period: null };
             if (!isDayPeriod(dayPeriod)) {
               throw new Error(`Invalid day/period format: ${trimmed}`);
             }
-            return dayPeriod;
+            return [dayPeriod];
           }
 
+          // コマをまたぐ場合
+          if (trimmed.match(/[~〜-]/)) {
+            const [start, end] = trimmed
+              .slice(1)
+              .split(/[~〜-]/)
+              .map(Number);
+            if (start === undefined || end === undefined) {
+              throw new Error(`Invalid period format: ${trimmed}`);
+            }
+            return Array.from({ length: end - start + 1 }, (_, i) => ({
+              day: trimmed[0],
+              period: start + i,
+            })).map((dayPeriod) => {
+              if (!isDayPeriod(dayPeriod)) {
+                throw new Error(
+                  `Invalid day/period format: ${dayPeriod.day} ${dayPeriod.period}`
+                );
+              }
+              return dayPeriod;
+            });
+          }
+
+          // 通常のコマ
           const day = trimmed[0];
-          const period = Number(trimmed[1]);
+          const period = Number(trimmed.slice(1));
           const dayPeriod = { day, period };
           if (!isDayPeriod(dayPeriod)) {
             throw new Error(`Invalid day/period format: ${s}`);
           }
-          return dayPeriod;
+          return [dayPeriod];
         });
     });
     return this.createParseResult(result, duration);

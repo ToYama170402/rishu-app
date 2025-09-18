2fca4c0de70dd491c89a9c288cfe712493e26957
ToYama170402 <yamato170402@gmail.com>
2025-09-18 03:51:21 +0000

    型の修正

--- syllabus/src/internal/models/day_period.go
+++ syllabus/src/internal/models/day_period.go
@@ -3,5 +3,5 @@ package models
 type DayPeriod struct {
 	DayPeriodID int    `gorm:"primaryKey;autoIncrement;column:day_period_id"`
 	Day         string `gorm:"column:day"`
-	Period      string `gorm:"column:period"`
+	Period      int    `gorm:"column:period"`
 }

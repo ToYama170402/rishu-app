583218f5ded7d0a93ca9ef78d377616283595168
ToYama <100946113+ToYama170402@users.noreply.github.com>
2025-09-23 15:06:56 +0900

    SSRがハイドレーションエラーになるので、syllabus-frontendは/builder/を受け付けるように変更 (#127)

--- syllabus-frontend/src/app.tsx
+++ syllabus-frontend/src/app.tsx
@@ -7,12 +7,13 @@ import "./app.css";
 export default function App() {
   return (
     <Router
-      root={props => (
+      root={(props) => (
         <>
           <Nav />
           <Suspense>{props.children}</Suspense>
         </>
       )}
+      base="/builder"
     >
       <FileRoutes />
     </Router>

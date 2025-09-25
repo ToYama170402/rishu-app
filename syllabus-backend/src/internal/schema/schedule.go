cf228f68c37d68fd008de6a6facc7ededcfa5a58
ToYama170402 <yamato170402@gmail.com>
2025-09-25 12:31:55 +0000

    インデックスを設定

--- syllabus-backend/src/internal/schema/schedule.go
+++ syllabus-backend/src/internal/schema/schedule.go
@@ -2,8 +2,8 @@ package schema
 
 type Schedule struct {
 	ScheduleID  int       `gorm:"primaryKey;autoIncrement;column:schedule_id"`
-	CourseID    int       `gorm:"column:course_id"`
+	CourseID    int       `gorm:"column:course_id;index"`
 	Course      Course    `gorm:"reference:CourseID"`
-	DayPeriodID int       `gorm:"column:day_period_id"`
+	DayPeriodID int       `gorm:"column:day_period_id;index"`
 	DayPeriod   DayPeriod `gorm:"reference:DayPeriodID"`
 }

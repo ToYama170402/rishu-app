79a8f9585bf776aeaae7cc3da51de75d751153e7
ToYama <100946113+ToYama170402@users.noreply.github.com>
2025-09-26 20:24:52 +0900

    DBに対する最適化 (#132)
    * サブクエリの利用から単純な結合に変更
    およそ10倍の高速化
    
    * インデックスを設定

--- syllabus-backend/src/internal/schema/course_semester_relation.go
+++ syllabus-backend/src/internal/schema/course_semester_relation.go
@@ -2,7 +2,7 @@ package schema
 
 type CourseSemesterRelation struct {
 	CourseSemesterRelationID int      `gorm:"primaryKey;column:course_semester_relation_id"`
-	CourseID                 int      `gorm:"column:course_id"`
+	CourseID                 int      `gorm:"column:course_id;index"`
 	Course                   Course   `gorm:"reference:CourseID"`
 	SemesterID               int      `gorm:"column:semester_id"`
 	Semester                 Semester `gorm:"reference:SemesterID"`

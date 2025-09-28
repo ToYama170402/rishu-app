032fa3a637120ddcdeddf76bb2ed8ee9b6adeefb
ToYama170402 <yamato170402@gmail.com>
2025-09-28 01:58:57 +0000

    サイドバーを実装

--- syllabus-frontend/src/app.tsx
+++ syllabus-frontend/src/app.tsx
@@ -3,6 +3,7 @@ import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import Nav from "@/components/Nav";
 import "./app.css";
+import { SideBarProvider } from "@/components/Nav";
 
 export default function App() {
   return (
@@ -12,9 +13,11 @@ export default function App() {
           <header>
             <Nav class="h-[44px]" />
           </header>
-          <main class="h-[calc(100vh-44px)] w-full">
-            <Suspense>{props.children}</Suspense>
-          </main>
+          <SideBarProvider>
+            <main class="h-[calc(100vh-44px)] w-full">
+              <Suspense>{props.children}</Suspense>
+            </main>
+          </SideBarProvider>
         </>
       )}
     >

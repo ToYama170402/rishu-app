eee96e34312423d24f9a83bd0edca901a899680f
ToYama <toyamasoujinn@gmail.com>
2025-11-09 13:10:26 +0000

    クエリを別ファイルに切り出し

--- syllabus-backend/src/internal/repositories/course_repositories.go
+++ syllabus-backend/src/internal/repositories/course_repositories.go
@@ -3,120 +3,21 @@ package repositories
 import (
 	"encoding/json"
 
+	_ "embed"
+
 	"github.com/ToYama170402/rishu-app/syllabus/src/internal/model"
 	"github.com/ToYama170402/rishu-app/syllabus/src/internal/schema"
 	"gorm.io/gorm"
 )
 
+//go:embed sql/course_list_json_query.sql
+var CourseListQuery string
+
 func GetCourses(db *gorm.DB) ([]model.Course, error) {
 	var rawResults []struct {
 		Data json.RawMessage `gorm:"column:jsonb_build_object"`
 	}
-	rawQuery := `
-SELECT
-  jsonb_build_object (
-    'courseId',
-    courses.course_id,
-    'year',
-    courses.year,
-    'title',
-    courses.title,
-    'numbering',
-    courses.numbering,
-    'courseNumber',
-    courses.course_number,
-    'numberOfProper',
-    courses.number_of_proper,
-    'semester',
-    json_agg (DISTINCT semesters.semester) FILTER (
-      WHERE
-        semesters.semester IS NOT NULL
-    ),
-    'numberOfCredits',
-    courses.number_of_credits,
-    'note',
-    courses.note,
-    'japaneseUrl',
-    courses.japanese_url,
-    'englishUrl',
-    courses.english_url,
-    'openAccount',
-    courses.open_account,
-    'max60CreditsFlag',
-    courses.max60_credits_flag,
-    'subjectDistinguished',
-    courses.subject_distinguished,
-    'courseDescription',
-    courses.course_description,
-    'instructors',
-    json_agg (
-      DISTINCT jsonb_build_object ('name', instructors.name)
-    ) FILTER (
-      WHERE
-        instructors.name IS NOT NULL
-    ),
-    'schedules',
-    json_agg (
-      DISTINCT jsonb_build_object (
-        'day',
-        day_periods.day,
-        'period',
-        day_periods.period
-      )
-    ) FILTER (
-      WHERE
-        day_periods.day IS NOT NULL
-        AND day_periods.period IS NOT NULL
-    ),
-    'classFormat',
-    class_formats.class_format,
-    'lectureForm',
-    lecture_forms.lecture_form,
-    'targetStudents',
-    target_students.target_students,
-    'lectureRoomInfo',
-    lecture_room_infos.lecture_room_info,
-    'faculty',
-    json_build_object (
-      'faculty',
-      faculties.faculty,
-      'department',
-      departments.department_name
-    ),
-    'keywords',
-    json_agg (DISTINCT keywords.keyword) FILTER (
-      WHERE
-        keywords.keyword IS NOT NULL
-    )
-  )
-FROM
-  courses
-  LEFT JOIN class_formats ON courses.class_format_id = class_formats.class_format_id
-  LEFT JOIN lecture_forms ON courses.lecture_form_id = lecture_forms.lecture_form_id
-  LEFT JOIN target_students ON courses.target_students_id = target_students.target_students_id
-  LEFT JOIN lecture_room_infos ON courses.lecture_room_info_id = lecture_room_infos.lecture_room_info_id
-  LEFT JOIN departments ON courses.department_id = departments.department_id
-  LEFT JOIN faculties ON departments.faculty_id = faculties.faculty_id
-  LEFT JOIN course_semester_relations on course_semester_relations.course_id = courses.course_id
-  LEFT JOIN semesters on course_semester_relations.semester_id = semesters.semester_id
-  LEFT JOIN responsibles on responsibles.course_id = courses.course_id
-  LEFT JOIN instructors on responsibles.instructor_id = instructors.instructor_id
-  LEFT JOIN schedules on courses.course_id = schedules.course_id
-  LEFT JOIN day_periods on schedules.day_period_id = day_periods.day_period_id
-  LEFT JOIN course_keyword_relations on course_keyword_relations.course_id = courses.course_id
-  LEFT JOIN keywords on keywords.keyword_id = course_keyword_relations.keyword_id
-GROUP BY
-  courses.course_id,
-  class_formats.class_format,
-  lecture_forms.lecture_form,
-  target_students.target_students,
-  lecture_room_infos.lecture_room_info,
-  faculties.faculty,
-  departments.department_name
-ORDER BY
-  courses.course_id;
-`
-	if err := db.Raw(rawQuery).Scan(&rawResults).Error; err != nil {
+	if err := db.Raw(CourseListQuery).Scan(&rawResults).Error; err != nil {
 		return nil, err
 	}
 

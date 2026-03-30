8dda3be60f29431d2a736cf92bec91bd621a5ef4
toyama170402 <toyamasoujinn@gmail.com>
2026-03-30 22:40:32 +0900

    legacyなフロントエンドを削除

--- legacy-frontend/src/const/endpoint.ts
+++
@@ -1,6 +0,0 @@
-const endpoint = {
-  TSV: "https://kurisyushien.org/api",
-  TSVDemoFromBrowser: "http://localhost:1313/dammy-data.tsv",
-  TSVDemoFromServer: "http://mock-page:1313/dammy-data.tsv",
-};
-export default endpoint;

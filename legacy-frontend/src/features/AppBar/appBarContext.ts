d3c38704a1a8855874718a9a1ff04de0d435aeb5
ToYama <100946113+ToYama170402@users.noreply.github.com>
2026-03-30 22:41:36 +0900

    legacyなフロントエンドを削除 (#204)

--- legacy-frontend/src/features/AppBar/appBarContext.ts
+++
@@ -1,3 +0,0 @@
-import { createContext, ReactNode, Dispatch, SetStateAction } from "react";
-
-export default createContext<Dispatch<SetStateAction<ReactNode>>>(() => {});
--- legacy-syllabus-frontend/src/type/instructor.ts
+++
@@ -1,3 +0,0 @@
-export type Instructor = {
-  name: string;
-};

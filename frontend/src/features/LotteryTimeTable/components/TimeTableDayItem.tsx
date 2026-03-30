a9bc8484bc7c348fea8cf666fe2738e225f4a1a3
toyama170402 <toyamasoujinn@gmail.com>
2026-03-30 12:33:05 +0900

    LotteryTimeTableの列の見出しと内容のあいだに謎の余白があったので削除

--- frontend/src/features/LotteryTimeTable/components/TimeTableDayItem.tsx
+++ frontend/src/features/LotteryTimeTable/components/TimeTableDayItem.tsx
@@ -11,7 +11,7 @@ export default function TimeTableDayItem({
   children,
 }: rowRendererProps<string>): React.ReactNode {
   return (
-    <Card className="mx-[2px] w-[calc(100%-2px)] xl:w-[calc(20%-4px)] lg:w-[calc(33.333%-4px)] md:w-[calc(50%-4px)] sm:w-[calc(100%-2px)] h-full overflow-hidden shrink-0 grow-0 py-1 px-0 snap-start scroll-ml-1">
+    <Card className="mx-[2px] gap-0 w-[calc(100%-2px)] xl:w-[calc(20%-4px)] lg:w-[calc(33.333%-4px)] md:w-[calc(50%-4px)] sm:w-[calc(100%-2px)] h-full overflow-hidden shrink-0 grow-0 py-1 px-0 snap-start scroll-ml-1">
       <CardHeader className="text-center p-0 border-b h-[20px] font-bold text-sm">
         {String(row)}
       </CardHeader>
